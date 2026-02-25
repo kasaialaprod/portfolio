d6c65ca183ce801d8a53b4e8893200171877978f
Kasai <wanzrael@gmail.com>
2026-02-25 12:34:07 +0100

    il faut que je retrouve le fil là mdr

--- public/world-page/school/javascript/chap1/lesson1/script.js
+++
@@ -1,4 +0,0 @@
-function congrateText() {
-    let text = document.getElementById("secretBox");
-    text.textContent = 'TU NE ME VOIS PLUS !!!';
-}
--- public/world-page/school/javascript/chap1/lesson2/script.js
+++
@@ -1,6 +0,0 @@
-function welcomeHero(event){
-    event.preventDefault(); // Annule le reload
-    let heroName = document.getElementById('heroName').value;
-    let welcomeMessage = document.getElementById('user-response');
-    welcomeMessage.textContent = "Bienvenu " + heroName;
-}
